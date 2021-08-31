b7ad2b177b070471acb9b70782b75be83ec72aa4
karimabuseer <karimabuseer@gmail.com>
2021-08-31 12:20:13 +0100

    Added withdraw confirmation message

--- lib/bank_helper.rb
+++ lib/bank_helper.rb
@@ -11,12 +11,23 @@ class BankHelper
 
   def deposit(amount)
     @balance += amount
+    deposit_confirmation(amount)
+  end
+
+  def deposit_confirmation(amount)
+    puts "You have successfully deposited £#{amount}."
   end
 
   def withdraw(amount)
     overdraft_check(amount)
     @balance -= amount
+    withdraw_confirmation(amount)
   end
+
+  def withdraw_confirmation(amount)
+    puts "You have successfully withdrawn £#{amount}."
+  end
+
 
   def overdraft_check(amount)
     raise "You do not have sufficient funds to complete this request." if amount > @balance
--- spec/bank_helper_spec.rb
+++ spec/bank_helper_spec.rb
@@ -16,6 +16,11 @@ describe BankHelper do
       subject.deposit(5)
       expect(subject.balance).to eq(5)
     end
+
+    it 'Succesfully depositing money gives me a confirmation' do
+      expect(STDOUT).to receive(:puts).with("You have successfully deposited £5.")
+      subject.deposit(5)
+    end
   end
 
   describe '#withdraw' do
@@ -27,6 +32,12 @@ describe BankHelper do
 
     it 'Prevents customers from withdrawing more than their account balance' do
       expect{ subject.withdraw(1) }.to raise_error("You do not have sufficient funds to complete this request.")
+    end
+
+    it 'notifies me when I succesfully withdraw money' do
+      subject.deposit(5)
+      expect(STDOUT).to receive(:puts).with("You have successfully withdrawn £3.")
+      subject.withdraw(3)
     end
   end
 
