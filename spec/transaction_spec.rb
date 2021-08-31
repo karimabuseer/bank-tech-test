12ce5e5bd861c24e4b3ac27ed48bb871aec9d512
karimabuseer <karimabuseer@gmail.com>
2021-08-31 17:41:44 +0100

    Testing framework for transaction

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,18 @@
+require 'transaction'
+describe Transaction do
+  it 'formats the date correctly' do
+
+  end
+
+  it 'formats the date correctly' do
+
+  end
+
+  it 'formats the date correctly' do
+
+  end
+
+  it 'produces a record of the transaction' do
+    
+  end
+end
